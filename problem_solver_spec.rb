a6d33987f94dbaced079ceecb94c6501837df3c3
Abdulaziz Fakhriddinov <mr_abdulaziz777@list.ru>
2023-07-14 20:23:59 +1000

    Create problem_solver_spec.rb file with tests in it

---
+++ problem_solver_spec.rb
@@ -0,0 +1,49 @@
+require './problem_solver'
+
+RSpec.describe Problem_Solver do
+  subject(:solver) { Problem_Solver.new }
+
+  describe '#factorial' do
+    it 'returns 1 when the input is 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'outputs the correct factorial for a positive integer' do
+      expect(solver.factorial(5)).to eq(120)
+      expect(solver.factorial(10)).to eq(3_628_800)
+    end
+
+    it 'alerts an ArgumentError for negative input' do
+      expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
+    end
+  end
+
+  describe '#reverse' do
+    it 'outputs the reversed word' do
+      expect(solver.reverse('hello')).to eq('olleh')
+      expect(solver.reverse('world')).to eq('dlrow')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'outputs "fizz" when the number is divisible by 3' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'outputs "buzz" when the number is divisible by 5' do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'outputs "fizzbuzz" when the number is divisible by both 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it 'outputs the number as a string for other cases' do
+      expect(solver.fizzbuzz(7)).to eq('7')
+      expect(solver.fizzbuzz(13)).to eq('13')
+    end
+  end
+end
